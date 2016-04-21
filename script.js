d97a9966b09a99b9f7289fde78d939ba2b64d6d3
Brody Berson <brody.berson@me.com>
2016-04-21 13:10:11 -0600

    Smooch Progress

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Script = require('smooch-bot').Script;
+const scriptRules = require('./script.json');
 
 module.exports = new Script({
     processing: {
@@ -20,15 +21,45 @@ module.exports = new Script({
         receive: (bot, message) => {
             const name = message.text;
             return bot.setProp('name', name)
-                .then(() => bot.say(`Great! Nice to meet you, ${name}`))
-                .then(() => 'finish');
+                .then(() => bot.say('Great! Nice to meet you, ${name}'))
+                .then(() => 'prompt');
+        }
+    },
+
+    prompt: {
+        receive: (bot) => {
+            return bot.say('Currently, I only respond to keywords. Please reply '+
+                'with one of the following: ')
+                .then(() => 'commands');
+        }
+    },
+
+    commands: {
+        receive: (bot, message) => {
+            var upperText = message.text.trim().toUpperCase();
+
+            if (!_.has(scriptRules, upperText)) {
+                    return bot.say('I didn\'t understand that – I can only respond to keywords ' +
+                        '(a word I\'ve said in all caps), like HELP.')
+                    .then(() => 'commands');
+                }
+            if (upperText == ('END' || 'BYE' || 'GOODBYE' || 'QUIT'))
+            {
+                return bot.say('Sorry to see you go.')
+                    .then(() => 'finish');
+            }else {
+                var response = scriptRules[upperText];
+                return bot.getProp('name')
+                    .then((name) => bot.say(response))
+                    .then(() => 'commands');
+            }
         }
     },
 
     finish: {
         receive: (bot, message) => {
             return bot.getProp('name')
-                .then((name) => bot.say(`Sorry ${name}, Brody is still working on me. ` +
+                .then((name) => bot.say('Sorry ${name}, Brody is still working on me. ' +
                         'Feel free to email him at brody.berson@me.com in the meantime...'))
                 .then(() => 'finish');
         }
